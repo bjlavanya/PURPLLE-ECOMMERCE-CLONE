4a2bcd904d25802892962cf00866ac01330dcbd7
bjlavanya <bjlavanya292004@gmail.com>
2026-04-20 11:23:09 +0530

    download invoice2

--- server/service/gstInvoicePdf.js
+++ server/service/gstInvoicePdf.js
@@ -58,7 +58,7 @@ const gstInvoicePdf = () => {
 
         doc.font('Helvetica-Bold')
             .fontSize(10)
-            .text('GSTIN.: ', 300,150, {align:'right',  width:550})
+            .text('GSTIN.: ', 300,150, {align:'right',  width:80})
 
         doc.font('Helvetica')
             .fontSize(10)
