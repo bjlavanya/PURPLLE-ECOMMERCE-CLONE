ca66b5eef6119e6880a71588c6750c3a9c4f94e6
bjlavanya <bjlavanya292004@gmail.com>
2026-04-20 10:37:50 +0530

    download invoice2

--- server/service/gstInvoicePdf.js
+++ server/service/gstInvoicePdf.js
@@ -15,11 +15,17 @@ const gstInvoicePdf = () => {
         //     .fontSize(20)
         //     .text('Thank you for your orders!!!', 100, 100)
 
-        doc.image('./image/purpllelogo.jpg', 50, 45, { width: 50 })
+        doc.image('./image/purpllelogo.jpg', 50, 45, { width: 100 })
             .fontSize(10)
             .text('101-B, 1st Floor, Raheja Plaza -I, LBS Marg', 200, 50, { align: 'right' })
             .text('Mumbai, Maharashtra, India, 400086', 200, 70, { align: 'right' })
             .moveDown();
+
+        doc.moveTo(50, 120)
+            .lineTo(550, 120)
+            .lineWidth(1)      
+            .strokeColor('#343434') 
+            .stroke();
 
         doc.end()
     })
