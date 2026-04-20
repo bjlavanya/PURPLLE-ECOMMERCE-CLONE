bee2d944820b20532853a5038c0a49f6a31bb2db
bjlavanya <bjlavanya292004@gmail.com>
2026-04-20 09:16:35 +0530

    Pdf designing

--- server/service/gstInvoicePdf.js
+++ server/service/gstInvoicePdf.js
@@ -15,8 +15,13 @@ const gstInvoicePdf = () => {
         //     .fontSize(20)
         //     .text('Thank you for your orders!!!', 100, 100)
 
-        doc.image('./image/purpllelogo.jpg', 0, 15, { width: 300 })
-            .text('Welcome to Purplle Invoice', 0, 0);
+        doc.image('./image/purpllelogo.jpg', 50, 45, { width: 50 })
+            .fontSize(20)
+            .text('ACME Inc.', 110, 57)
+            .fontSize(10)
+            .text('101-B, 1st Floor, Raheja Plaza -I, LBS Marg', 200, 65, { align: 'right' })
+            .text('Mumbai, Maharashtra, India, 400086', 200, 80, { align: 'right' })
+            .moveDown();
 
         doc.end()
     })
